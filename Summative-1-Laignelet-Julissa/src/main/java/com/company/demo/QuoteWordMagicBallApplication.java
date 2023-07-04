213e8a35b94cd1ffc39222e2a992e74bc406d746
jal1077 <jal1077@nyu.edu>
2023-07-03 21:10:24 -0400

    first creation - start.spring.io

---
+++ Summative-1-Laignelet-Julissa/src/main/java/com/company/demo/QuoteWordMagicBallApplication.java
@@ -0,0 +1,13 @@
+package com.company.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteWordMagicBallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteWordMagicBallApplication.class, args);
+	}
+
+}
